322fd2839d1c24b95bf9d1dd5c6ba91957e7202b
Alastair Porter <alastair.porter@upf.edu>
2019-07-29 17:01:48 +0200

    Format source files, update docstrings

--- process_pub_database/get_data_cpdl.py
+++ process_pub_database/get_data_cpdl.py
@@ -1,131 +1,116 @@
-import mediawiki
-import pandas as pd
-from bs4 import BeautifulSoup as bs
-import sys
-import os,re
-import json
-import urllib.request
-import time
+import re
 import unicodedata
 
+from bs4 import BeautifulSoup as bs
 from utils import *
 
 
 def find_composer(bs_file):
+    """find the name and URL of a composer from an HTML page.
+    Returns:
+        a dictionary containing the name of the composer and the URL
     """
-    Function to find the name and URL of a composer from an HTML page.
-    Returns a dictionary containing the name of the composer and the URL
-    """
-    compy = bs_file.find_all('b',text = re.compile('Composer|Editors'))[0].find_next_siblings('a')[0]
+    compy = bs_file.find_all('b', text=re.compile('Composer|Editors'))[0].find_next_siblings('a')[0]
     compy_name = compy['title']
     compy_name = unicodedata.normalize('NFKC', compy_name).strip()
-    composer = {'Name' : compy_name, 
-    'url':'https://www.cpdl.org'+compy['href']}
+    composer = {'Name': compy_name,
+                'url': 'https://www.cpdl.org' + compy['href']}
     return composer
 
+
 def process_composer(comp_str):
-    """
-    Function to extract information about a composer from an HTML page. 
-    Returns a dictionary with the name, url, biography, wikipedia link, date of birth and date of death of the composer.
+    """Extract information about a composer from an HTML page.
+
+    Returns:
+         a dictionary with the name, url, biography, wikipedia link, date of birth and date of death of the composer.
     """
 
     composer_file = bs(comp_str, features="lxml")
 
-    dict_comp = {}
-
     if 'Biography' in comp_str:
-        biop = composer_file.find_all('b',text = re.compile('Biography'))[0].parent
-        bio = biop.getText().rstrip()+biop.find_next_siblings()[0].getText().strip()
-        bio = bio.replace('Biography','').replace(':','').strip()
+        biop = composer_file.find_all('b', text=re.compile('Biography'))[0].parent
+        bio = biop.getText().rstrip() + biop.find_next_siblings()[0].getText().strip()
+        bio = bio.replace('Biography', '').replace(':', '').strip()
     elif 'Wikipedia article' in comp_str:
-        bio = composer_file.find_all(text = re.compile('Wikipedia article'))[0].parent.find_previous_siblings('p')[0].getText().strip()
+        bio = composer_file.find_all(text=re.compile('Wikipedia article'))[0].parent.find_previous_siblings('p')[
+            0].getText().strip()
     if 'Wikipedia article' in comp_str:
 
-        wiki_link = composer_file.find_all(text = re.compile('Wikipedia article'))[0].parent.find('a')['href']
-    else: 
+        wiki_link = composer_file.find_all(text=re.compile('Wikipedia article'))[0].parent.find('a')['href']
+    else:
         wiki_link = ''
 
     if 'Born' in comp_str:
-        born = composer_file.find_all(text = re.compile('Born'))[0].parent.next_sibling.string.strip()
+        born = composer_file.find_all(text=re.compile('Born'))[0].parent.next_sibling.string.strip()
     else:
         born = 'Unknown'
     if 'Died' in comp_str:
-        died = composer_file.find_all(text = re.compile('Died'))[0].parent.next_sibling.string.strip()
+        died = composer_file.find_all(text=re.compile('Died'))[0].parent.next_sibling.string.strip()
     else:
         died = 'Unknown'
 
-    dict_comp['Biography'] = bio
-    dict_comp['Wikipedia'] = wiki_link
-    dict_comp['Born'] = born
-    dict_comp['Died'] = died
+    return {'Biography': bio, 'Wikipedia': wiki_link, 'Born': born, 'Died': died}
 
-    return dict_comp
 
 def find_name(title_file):
+    """Find the title of the work.
+
+    Returns:
+        a string with the normalized name of work.
     """
-    Function to find the title of the work.
-    Returns a string with the normalized name of work.
-    """
-    name = title_file.find_all(text = re.compile('Title:'))[0].parent.contents
-    if len(name)>1:
+    name = title_file.find_all(text=re.compile('Title:'))[0].parent.contents
+    if len(name) > 1:
         name = name[-1]
     else:
-        name = title_file.find_all('b',text = re.compile('Title:'))[0].parent.contents
-        if len(name)>1:
+        name = title_file.find_all('b', text=re.compile('Title:'))[0].parent.contents
+        if len(name) > 1:
             if name[1].isspace():
                 name = name[2].string
             else:
                 name = name[1].string
         else:
-            name = title_file.find_all('b',text = re.compile('Title:'))[0].find_next_siblings()[0].string
+            name = title_file.find_all('b', text=re.compile('Title:'))[0].find_next_siblings()[0].string
 
     name = unicodedata.normalize('NFKC', name).strip()
 
     return name
 
+
 def find_mxl(bs_file):
-    """
-    Function to find the MXL file from an HTML page. 
-    Retruns a dictionary with the link to the MXL file, 
-    the name of the publisher, with URL of the publisher, the associated liscence and the format. 
+    """find the MXL file from an HTML page.
+
+    Returns: a dictionary with the link to the MXL file,
+            the name of the publisher, with URL of the publisher,
+            the associated license and the format.
     """
     page_text = bs_file.getText()
     if 'mxl' in page_text:
-        mxl = bs_file.find_all('a',href = re.compile('mxl'))[0]
+        mxl = bs_file.find_all('a', href=re.compile('mxl'))[0]
         formatmaxl = 'application/vnd.recordare.musicxml'
-    elif 'XML' in page_text: 
-        mxl = bs_file.find_all('a',href = re.compile('XML'))[0]
+    elif 'XML' in page_text:
+        mxl = bs_file.find_all('a', href=re.compile('XML'))[0]
         formatmaxl = 'application/vnd.recordare.musicxml+xml'
-    elif 'xml' in page_text: 
-        mxl = bs_file.find_all('a',href = re.compile('xml'))[0]
+    elif 'xml' in page_text:
+        mxl = bs_file.find_all('a', href=re.compile('xml'))[0]
         formatmaxl = 'application/vnd.recordare.musicxml+xml'
     elif 'MXL' in page_text:
-        mxl = bs_file.find_all('a',href = re.compile('MXL'))[0]
+        mxl = bs_file.find_all('a', href=re.compile('MXL'))[0]
         formatmaxl = 'application/vnd.recordare.musicxml'
     mxl_link = mxl['href']
     editory = mxl.parent.parent.next_sibling.next_sibling
-    notes = editory.find('b', text = re.compile('notes')).next_sibling.parent.getText()
-    
-    editorx = editory.find('b', text = re.compile('Editor')).find_next_siblings('a')[0]
+    notes = editory.find('b', text=re.compile('notes')).next_sibling.parent.getText()
 
-    editor = {'Name': editorx.getText(), 'url': 'https://www.cpdl.org'+editorx['href']}
+    editorx = editory.find('b', text=re.compile('Editor')).find_next_siblings('a')[0]
 
-    m_link_dict = {}
+    editor = {'Name': editorx.getText(), 'url': 'https://www.cpdl.org' + editorx['href']}
 
-    m_link_dict['Publisher'] = editor['Name']
-
-    m_link_dict['Publisher_url'] = editor['url']
-
-    m_link_dict['File_url'] = mxl_link
-
-    m_link_dict['License'] = license
-
-    m_link_dict['Format'] = formatmaxl
+    m_link_dict = {'Publisher': editor['Name'], 'Publisher_url': editor['url'], 'File_url': mxl_link,
+                   'License': license, 'Format': formatmaxl}
 
     return m_link_dict, notes
 
-def main():
 
+def main():
     list_of_titles, md = get_list_of_titles('http://www.cpdl.org/wiki/api.php', "4-part choral music")
 
     print('Info retrieved')
@@ -140,11 +125,10 @@ def main():
     composers = []
     composer_dict = {}
 
-
     for title in list_of_titles:
 
-        try: 
-            count_total+=1
+        try:
+            count_total += 1
 
             process, source = check_mxl(md, title, ['MusicXML'])
 
@@ -156,16 +140,15 @@ def main():
                     license = 'https://www0.cpdl.org/wiki/index.php/ChoralWiki:CPDL'
                 else:
                     licence = 'Unknown'
-                
+
                 dict_file = {}
-                
+
                 title_file = bs(title_str, features="lxml")
                 composer = find_composer(title_file)
 
                 if composer not in composers:
                     composers.append(composer)
                     if not composer['Name'] == 'Anonymous':
-
                         comp_str = read_source(composer['url'])
                         dict_comp = process_composer(comp_str)
                         dict_comp['Source'] = composer['url']
@@ -176,11 +159,12 @@ def main():
 
                 global_description = "Composition {} by {}.".format(name.strip(), composer['Name'].strip())
 
-                gd = "MusicXML score for {}.".format(name.strip()) 
+                gd = "MusicXML score for {}.".format(name.strip())
 
                 if 'Description' in title_file.getText():
 
-                    local_description = title_file.find_all('b',text = re.compile('Description'))[0].parent.get_text().replace('Description:','').strip()
+                    local_description = title_file.find_all('b', text=re.compile('Description'))[
+                        0].parent.get_text().replace('Description:', '').strip()
 
                     local_description = unicodedata.normalize('NFKC', local_description).strip()
 
@@ -190,7 +174,7 @@ def main():
                 else:
                     description = global_description
 
-                language = title_file.find_all('b',text = re.compile('Language'))[0].find_next_siblings()[0].string
+                language = title_file.find_all('b', text=re.compile('Language'))[0].find_next_siblings()[0].string
                 if language == 'None':
                     language = ''
 
@@ -210,20 +194,21 @@ def main():
                 dict_file['Contributor'] = 'https://www.upf.edu'
                 dict_file['Relation'] = m_links_out
                 dict_file['Language'] = 'en'
-                
-                dict_file['Subject'] = language.strip()+' choir piece'
+
+                dict_file['Subject'] = language.strip() + ' choir piece'
 
                 dict_all[title] = dict_file
 
-                count_xml+=1
+                count_xml += 1
 
-        except Exception as e: 
-            count_except+=1
+        except Exception as e:
+            count_except += 1
             print('Fail {}'.format(count_except))
             fails.append(title.encode('utf-8'))
-        progress(count_total,len(list_of_titles), " {} files done".format(count_xml))
+        progress(count_total, len(list_of_titles), " {} files done".format(count_xml))
     write_json(dict_all, './cpdl.json')
     write_json(composer_dict, './cpdl_composers.json')
+
 
 if __name__ == '__main__':
     main()
--- process_pub_database/get_data_imslp.py
+++ process_pub_database/get_data_imslp.py
@@ -1,37 +1,31 @@
-import mediawiki
-import pandas as pd
-from bs4 import BeautifulSoup as bs
-import sys
-import os,re
-import json
-import urllib.request
+process_pub_database/get_data_imslp.pyimport re
 import unicodedata
 
+from bs4 import BeautifulSoup as bs
 from utils import *
 
+
 def find_composer(bs_file):
-    """
-    Function to find the name and URL of a composer from an HTML page.
-    Returns a dictionary containing the name of the composer and the URL
-    """
-    composer = bs_file.find_all(text = 'Composer\n')[0].parent.parent.find('td').contents[0]
+    """Find the name and URL of a composer from an HTML page.
 
+    Returns:
+        a dictionary containing the name of the composer and the URL
+    """
+    composer = bs_file.find_all(text='Composer\n')[0].parent.parent.find('td').contents[0]
     composer_name = composer.getText()
-
     composer_url = 'https://imslp.org' + composer['href']
-
-    composer_name = composer_name.split(',')[1][1:]+' '+ composer_name.split(',')[0]
-
+    composer_name = composer_name.split(',')[1][1:] + ' ' + composer_name.split(',')[0]
     composer_name = unicodedata.normalize('NFKC', composer_name).strip()
-
-    composer = {'Name' : composer_name, 'url': composer_url}
+    composer = {'Name': composer_name, 'url': composer_url}
 
     return composer
 
+
 def process_composer(composer):
-    """
-    Function to extract information about a composer from an HTML page. 
-    Returns a dictionary with the name, url, biography, wikipedia link, date of birth and date of death of the composer.
+    """Extract information about a composer from an HTML page.
+
+    Returns:
+        a dictionary with the name, url, biography, wikipedia link, date of birth and date of death of the composer.
     """
 
     dict_comp = {}
@@ -41,88 +35,94 @@ def process_composer(composer):
     composer_file = bs(comp_str, features="lxml")
 
     if 'Wikipedia' in composer_file.getText():
-        wiki_link = composer_file.find_all('a',text = 'Wikipedia')[0]['href']
+        wiki_link = composer_file.find_all('a', text='Wikipedia')[0]['href']
         dict_comp['Wikipedia'] = wiki_link
 
     dict_comp['Source'] = composer['url']
-    
+
     dict_comp['Title'] = composer['Name']
 
     return dict_comp
 
+
 def find_name(title_file):
+    """Find the title of the work.
+
+    Returns:
+        a string with the normalized name of work.
     """
-    Function to find the title of the work.
-    Returns a string with the normalized name of work.
-    """
-    name = title_file.find_all(text = re.compile('Work Title'))[0].parent.parent.find('td').contents[0].string
+    name = title_file.find_all(text=re.compile('Work Title'))[0].parent.parent.find('td').contents[0].string
 
     name = unicodedata.normalize('NFKC', name).strip()
 
     return name
 
+
 def find_lang(title_file):
-    """
-    Function to find the title of the work.
-    Returns a string with the normalized name of work.
+    """Find the title of the work.
+
+    Returns:
+        a string with the normalized name of work.
     """
     if 'Language' in title_file.getText():
-        language = title_file.find_all(text = re.compile('Language'))[0].parent.parent.find('td').contents[0].string
+        language = title_file.find_all(text=re.compile('Language'))[0].parent.parent.find('td').contents[0].string
 
     else:
         language = 'Unknown'
 
     return language
 
+
 def find_mxl_links(title_file, name):
-    mxl_links = [x.parent.parent for x in title_file.find_all(text = re.compile('XML')) if x.parent.parent.name == 'a' ] 
+    mxl_links = [x.parent.parent for x in title_file.find_all(text=re.compile('XML')) if x.parent.parent.name == 'a']
     mxl_links_out = []
 
     for m_link in mxl_links:
         m_link_dict = {}
 
         mxl_link = m_link['href']
-        
+
         mxl_str = read_source(mxl_link)
 
         mxl_bs = bs(mxl_str, features="lxml")
-        mxl_final_link = 'https://imslp.org'+mxl_bs.find_all('a',text = re.compile('download'))[0]['href']
+        mxl_final_link = 'https://imslp.org' + mxl_bs.find_all('a', text=re.compile('download'))[0]['href']
 
         link_parent = mxl_links[0].parent.parent.parent.parent.parent
 
-        pubs = link_parent.find_all(text = re.compile('Arranger|Editor'))[0].parent.parent.find('td').find('a')
+        pubs = link_parent.find_all(text=re.compile('Arranger|Editor'))[0].parent.parent.find('td').find('a')
 
         m_link_dict['Publisher'] = pubs.getText()
 
         m_link_dict['Publisher_url'] = 'https://imslp.org' + pubs['href']
 
-        m_link_dict['File_url'] = mxl_final_link 
+        m_link_dict['File_url'] = mxl_final_link
 
-        m_link_dict['License'] = 'https://imslp.org' + link_parent.find_all(text = re.compile('Copy'))[0].parent.parent.find('td').find_all('a')[0]['href']
+        m_link_dict['License'] = 'https://imslp.org' + \
+                                 link_parent.find_all(text=re.compile('Copy'))[0].parent.parent.find('td').find_all(
+                                     'a')[0]['href']
 
         m_link_dict['Format'] = 'application/zip'
 
         global_desc = "MusicXML score for {} ".format(name.strip())
 
         if 'Misc. Notes' in link_parent.getText():
-
-            pub_desc = link_parent.find_all(text = re.compile('Misc. Notes'))[0].parent.parent.getText().replace('\n','').replace('Misc. Notes','')
+            pub_desc = link_parent.find_all(text=re.compile('Misc. Notes'))[0].parent.parent.getText().replace('\n',
+                                                                                                               '').replace(
+                'Misc. Notes', '')
 
             pub_desc = unicodedata.normalize('NFKC', pub_desc).strip()
 
-            m_link_dict['Description'] =global_desc + pub_desc
+            m_link_dict['Description'] = global_desc + pub_desc
 
         mxl_links_out.append(m_link_dict)
 
         return mxl_links_out
 
-def main():
 
+def main():
     list_of_titles, md = get_list_of_titles('https://imslp.org/api.php', "For unaccompanied chorus")
 
-
     print('Info retrieved')
-
 
     count_xml = 0
     count_target = 0
@@ -132,12 +132,12 @@ def main():
     fails = []
     dict_all = {}
 
-    composers =[]
+    composers = []
 
     composer_dict = {}
 
     for title in list_of_titles:
-        count_total+=1
+        count_total += 1
 
         try:
             # title = 'Et respondens Iesus dixit illis (Lange, Gregor)'
@@ -152,7 +152,7 @@ def main():
                 title_file = bs(title_str, features="lxml")
 
                 composer = find_composer(title_file)
-                
+
                 if composer not in composers:
                     composers.append(composer)
 
@@ -173,23 +173,24 @@ def main():
                 dict_file['Contributor'] = 'https://www.upf.edu'
                 dict_file['Relation'] = mxl_links_out
                 dict_file['Language'] = 'en'
-                
-                dict_file['Subject'] = language.replace('\n','')+' choir piece'
+
+                dict_file['Subject'] = language.replace('\n', '') + ' choir piece'
 
                 dict_all[title] = dict_file
-                count_xml+=1
+                count_xml += 1
 
-        except Exception as e: 
+        except Exception as e:
             print(title.encode('utf-8'))
             print(e)
-            count_except+=1
+            count_except += 1
             print('Fail {}'.format(count_except))
 
             fails.append(title)
-            
+
         progress(count_total, len(list_of_titles), " {} files done".format(count_xml))
     write_json(dict_all, './imslp.json')
     write_json(composer_dict, './imslp_composers.json')
+
 
 if __name__ == '__main__':
     main()
