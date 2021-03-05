fd9df7d16e58a3e942b4fdb69d44d00be32c6415
Alastair Porter <alastair.porter@upf.edu>
2021-03-05 20:50:16 +0100

    Use artist.as_dict() to simplify code

--- importers/artist.py
+++ importers/artist.py
@@ -1,11 +1,12 @@
 """
 Muziekweb artist importer
 """
-from datetime import date
+from typing import Optional
 
 import trompace as ce
 from SPARQLWrapper import SPARQLWrapper, JSON
 from trompace.connection import submit_query
+from trompace.mutations.person import mutation_update_person, mutation_create_person
 
 from models import CE_Person
 from trompace_local import GLOBAL_CONTRIBUTOR, GLOBAL_IMPORTER_REPO, GLOBAL_PUBLISHER, lookupIdentifier
@@ -18,7 +19,7 @@ async def import_artist(keys: list):
     for key in keys:
         print(f"Retrieving artist with key {key} from Muziekweb")
         # Get data from Muziekweb
-        artist = await get_mw_artist_sparql(key)
+        artist = await get_mw_artist(key)
 
         if artist is None:
             print(f"No data received for {key}")
@@ -28,74 +29,11 @@ async def import_artist(keys: list):
 
         if artist.identifier is not None:
             print(f"Updating record {artist.identifier} in Trompa CE", end="")
-            response = await ce.connection.submit_query(mutation_update_artist(
-                identifier=artist.identifier,
-                artist_name=artist.name,
-                publisher=artist.publisher,
-                contributor=artist.contributor,
-                creator=artist.creator,
-                source=artist.source,
-                description=artist.description,
-                language=artist.language,
-                coverage=None,
-                # formatin="text/html",
-                date=date.today(),
-                disambiguatingDescription=artist.disambiguatingDescription,
-                relation=artist.relatedTo,
-                _type=None,
-                _searchScore=None,
-                additionalType=artist.additionalType,
-                alternateName=artist.alternateName,
-                image=artist.image,
-                sameAs=artist.sameAs,
-                url=artist.url,
-                additionalName=artist.additionalName,
-                award=artist.award,
-                birthDate=artist.birthDate,
-                deathDate=artist.deathDate,
-                familyName=artist.familyName,
-                gender=artist.gender,
-                givenName=artist.givenName,
-                honorificPrefix=artist.honorificPrefix,
-                honorificSuffix=artist.honorificSuffix,
-                jobTitle=artist.jobTitle,
-                knowsLanguage=artist.knowsLanguage
-            ))
+            response = await ce.connection.submit_query(mutation_update_person(**artist.as_dict()))
             artist.identifier = response["data"]["UpdatePerson"]["identifier"]
         else:
             print("Inserting new record in Trompa CE", end="")
-            response = await ce.connection.submit_query(mutation_create_artist(
-                artist_name=artist.name,
-                publisher=artist.publisher,
-                contributor=artist.contributor,
-                creator=artist.creator,
-                source=artist.source,
-                description=artist.description,
-                language=artist.language,
-                coverage=None,
-                # formatin="text/html",
-                date=date.today(),
-                disambiguatingDescription=artist.disambiguatingDescription,
-                relation=artist.relatedTo,
-                _type=None,
-                _searchScore=None,
-                additionalType=artist.additionalType,
-                alternateName=artist.alternateName,
-                image=artist.image,
-                sameAs=artist.sameAs,
-                url=artist.url,
-                additionalName=artist.additionalName,
-                award=artist.award,
-                birthDate=artist.birthDate,
-                deathDate=artist.deathDate,
-                familyName=artist.familyName,
-                gender=artist.gender,
-                givenName=artist.givenName,
-                honorificPrefix=artist.honorificPrefix,
-                honorificSuffix=artist.honorificSuffix,
-                jobTitle=artist.jobTitle,
-                knowsLanguage=artist.knowsLanguage
-            ))
+            response = await ce.connection.submit_query(mutation_create_person(**artist.as_dict()))
             artist.identifier = response["data"]["CreatePerson"]["identifier"]
 
         if artist.identifier is None:
@@ -106,7 +44,7 @@ async def import_artist(keys: list):
     print("Importing artists done.")
 
 
-async def get_mw_artist(key: str) -> CE_Person:
+async def get_mw_artist(key: str) -> Optional[CE_Person]:
     sparql = SPARQLWrapper(
         "https://api.data.muziekweb.nl/datasets/muziekweborganization/Muziekweb/services/Muziekweb/sparql")
     sparql.setReturnFormat(JSON)
